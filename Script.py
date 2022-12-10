86f5ae73df9c3ec5d899f35931303743e45996a5
MikeTG000 <119830644+MikeTG000@users.noreply.github.com>
2022-12-10 20:29:16 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,161 +4,77 @@ class script(object):
 𝖨𝗆 𝖺𝗇 𝖺𝗎𝗍𝗈 𝖿𝗂𝗅𝗍𝖾𝗋 𝖻𝗈𝗍 𝗐𝗁𝗂𝖼𝗁 𝖼𝖺𝗇 𝗉𝗋𝗈𝗏𝗂𝖽𝖾 𝗆𝗈𝗏𝗂𝖾𝗌 𝗂𝗇 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉𝗌. 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 𝖺𝗇𝖽 𝗉𝗋𝗈𝗆𝗈𝗍𝖾 𝗆𝖾 𝖺𝗌 𝖺𝖽𝗆𝗂𝗇 𝗍𝗈 𝗅𝖾𝗍 𝗆𝖾 𝗀𝖾𝗍 𝗂𝗇 𝖺𝖼𝗍𝗂𝗈𝗇.
 
 𝖢𝗅𝗂𝖼𝗄 𝗈𝗇 𝗍𝗁𝖾 𝖧𝖾𝗅𝗉 𝖻𝗎𝗍𝗍𝗈𝗇 𝖿𝗈𝗋 𝖬𝗈𝗋𝖾..."""
-    HELP_TXT = """<b>Welcome To My Help Module #1</b>"""
-    ONE_TXT = """<b>Welcome To My Help Module #2</b>"""
-    TWO_TXT = """<b>Welcome To My Help Module #3</b>"""
-    BIO_TXT = """<b>○ My Name: Michael Jackson
-○ Developer: <a href=https://t.me/ddrabit/19>LᴀL</a>
-○ Language: Phython3
-○ Data Base: MongoDB
-○ Bot Server: Heroku
-○ Build Status: v1.0.2 [ Beta ]</b>"""
-    ABOUT_TXT = """<b>• ɴᴀᴍᴇ: ᴍɪᴄʜᴀᴇʟ ᴊᴀᴄᴋsᴏɴ</b>
-<b>• ᴏᴡɴᴇʀ: LᴀL</b>
-<b>• ʟɪʙʀᴀʀʏ: ᴘʏʀᴏɢʀᴀᴍ</b>
-<b>• ʟᴀɴɢᴜᴀɢᴇ: ᴘʏᴛʜᴏɴ 3</b>
-<b>• ᴅᴀᴛᴀʙᴀsᴇ: ᴍᴏɴɢᴏ ᴅʙ</b>
-<b>• ʙᴏᴛ sᴇʀᴠᴇʀ: ʜᴇʀᴏᴋᴜ</b>
-<b></b>"""
-    BATCH_TXT = """ 
-<b>BATCH</b>
+    HELP_TXT = """<b>Welcome To My Help Module</b>"""
+    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/LᴀL>ddrabit</a>
+✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
+✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: KOYEB
+✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+    SOURCE_TXT = """<b>NOTE:</b>
+- TechMagazine-AutoFilterBot is a open source project. 
+- Source - https://github.com/itsyogieu/TechMagazine-AutoFilterBot
 
-You can use the filestore feature for any public channel without bot being admin in that channel (only media messages  can be stored). 
+<b>DEVS:</b>
+- <a href=https://t.me/TechMagazineYT>TechMagazine</a>"""
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-</b>Commands & Usage:</b>
+- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
-1. For a single file use /link command as reply to file.(only video, audio and documents are supported for now.)
+<b>NOTE:</b>
+1. TechMagazine-AutoFilterBot should have admin privillage.
+2. only admins can add filters in a chat.
+3. alert buttons have a limit of 64 characters.
 
-2. For creating batch files , use /batch </code><starting message link> <ending message link>.</code>
-Example: </code/batch https://t.me/teamEvaMaria/2</code> </code>https://t.me/teamEvaMaria/9</code>
-
-"""
-    CARB_TXT = """
-
-<b>CARBON</b>
-
-/carbon - <code>reply to a text for carbon image.</code>
- """
-    SHARE_TXT = """
-<b>SHARE TEXT</b>
-
-This feature helps you to get your text into sharable link. 
-
-/share - <code>Reply to a text to generate sharable link.</code>
- """
-    IMDB_TXT = """
-<b>IMDB</b>
-
-Search IMDB results and customize IMDB Template. You can customize them as per your needs.
-
-<b>Comands & Usage:</b>
-
-/imdb  - <code>get the film information from IMDb source.</code>
-/set_template - <code>To set Custom template for your group.</code>
-
-<a href=https://t.me/TeamEvamaria/9>Know More....</a>"""
-    WHOIS_TXT ="""<b>Commands and Usage</b>
-
-/whois - For user details"""
-    FUN_TXT ="""<b>Games</b> 
-    
-𝟣. /dice - Role The Dice
-𝟤. /Throw 𝗈𝗋 /Dart - To Make Dart 
-3. /Runs - Some Random Dialogues
-4. /Goal or /Shoot - To Make a Goal or Shoot
-5. /luck or /cownd - Spin And Try Your Luck"""
-    DEPLOY_TXT = """<b>𝙷𝙾𝚆 𝚃𝙾 𝙳𝙴𝙿𝙻𝙾𝚈..? 
-  
-<b>✮ Deploy Tutorial ››</b> <i><b>https://youtu.be/kB9TkCs8cX0</b></i>
-
-<b>𝙸𝙵 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃 𝚃𝙷𝙴 𝙰𝙹𝙰𝚇-𝙿𝚁𝙾-𝙼𝙰𝚇 𝚁𝙴𝙿𝙾 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 <a href=https://t.me/AboutAadhi>𝙰𝙰𝙳𝙷𝙸</a></b>
-
-<b>𝚂𝙷𝙰𝚁𝙴 𝙰𝙽𝙳 𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴</b>
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/MWUpdatez><b>𝙼𝚆-𝚄𝙿𝙳𝙰𝚃𝙴𝚉</b></a>"""
-    MANUELFILTER_TXT = """<b>Commands and Usage.</b>
-
+<b>Commands and Usage:</b>
 • /filter - <code>add a filter in chat</code>
 • /filters - <code>list all the filters of a chat</code>
 • /del - <code>delete a specific filter in chat</code>
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    SONG_TXT = """<b>Commands and Usage.</b>
+    BUTTON_TXT = """Help: <b>Buttons</b>
 
-</song Song Name>
+- TechMagazine-AutoFilterBot Supports both url and alert inline buttons.
 
-📌 ᴡᴏʀᴋs ᴏɴʟʏ ᴏɴ ɢʀᴏᴜᴘs."""
-    PIN_TXT ="""<b>Commands and Usage.</b>
+<b>NOTE:</b>
+1. Telegram will not allows you to send buttons without any content, so content is mandatory.
+2. TechMagazine-AutoFilterBot supports buttons with any telegram media type.
+3. Buttons should be properly parsed as markdown format
 
-/pin - To Pin The Message On Your Chat.
-/unpin - To Unpin The Current Pinned Message."""
-    AUTOFILTER_TXT = """<b>Help: Auto Filter</b>
+<b>URL buttons:</b>
+<code>[Button Text](buttonurl:https://t.me/TechMagazineYT)</code>
+
+<b>Alert buttons:</b>
+<code>[Button Text](buttonalert:This is an alert message)</code>"""
+    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
 
 <b>NOTE:</b>
 1. Make me the admin of your channel if it's private.
 2. make sure that your channel does not contains camrips, porn and fake files.
 3. Forward the last message to me with quotes.
  I'll add all the files in that channel to my db."""
-    PASTE_TXT = """<b>Commands and Usage.</b>
+    CONNECTION_TXT = """Help: <b>Connections</b>
 
-/paste [text] - paste the given text on Pasty
-
-<b>NOTE:</b>
-
-• These commands works on both pm and group.
-• These commands can be used by any group member."""
-    TTS_TXT = """<b>Commands and Usage.</b>
-
-/tts <text> : convert text to speech
+- Used to connect bot to PM for managing filters 
+- it helps to avoid spamming in groups.
 
 <b>NOTE:</b>
+1. Only admins can add a connection.
+2. Send <code>/connect</code> for connecting me to ur PM
 
-• These commands works on both pm and group.
-• IMDb can translate texts to 200+ languages."""
-    PINGS_TXT ="""<b>Usage.</b>
-
-/ping - To get your ping."""
-    TELE_TXT = """<b>Commands and Usage.</b>
-
-/telegraph - Send me Picture or Video Under (5MB)
-
-<b>NOTE:</b>
-
-This Command Is Available in goups and pms.
-This Command Can be used by everyone."""
-
-    PRIVATEBOT_TXT = """Hey {} I'm Alive."""
-
-    JSON_TXT ="""<b>JSON:</b>
-
-Bot returns json for all replied messages with /json
-
-<b>Features:</b>
-
-Message Editting JSON.
-Pm Support.
-Group Support."""
-    PURGE_TXT = """<b>Usage.</b>
-   
-/purge :- Delete All Messages From The Replied To Message, To The Current Message"""
-    BUTTON_TXT = """<b>Usage.</b>
-
-<b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/)</code>
-
-<b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUT_TXT = """<b>Commands and Usage.</b>
-
-/autofilter on - Enable auto filter.
-/autofilter off - Disable auto filter.
-/set_template - Set custom ɪᴍᴅʙ template."""
-    CONNECTION_TXT = """<b>Commands and Usage:</b>
+<b>Commands and Usage:</b>
 • /connect  - <code>connect a particular chat to your PM</code>
 • /disconnect  - <code>disconnect from a chat</code>
 • /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """<b>Commands and Usage:</b>
+    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
-• /id - <code>get id of a specifed user.</code>
+<b>NOTE:</b>
+these are the extra features of Eva Maria
+
+<b>Commands and Usage:</b>
+• /id - <code>get id of a specified user.</code>
 • /info  - <code>get information about a user.</code>
+• /imdb  - <code>get the film information from IMDb source.</code>
 • /search  - <code>get the film information from various sources.</code>"""
     ADMIN_TXT = """Help: <b>Admin mods</b>
 
@@ -173,127 +89,21 @@ This module only works for my admins
 • /chats - <code>to get list of the my chats and ids </code>
 • /leave  - <code>to leave from a chat.</code>
 • /disable  -  <code>do disable a chat.</code>
-• /ban_user  - <code>to ban a user.</code>
-• /unban_user  - <code>to unban a user.</code>
+• /ban  - <code>to ban a user.</code>
+• /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """<b>📁 ᴛᴏᴛᴀʟ ғɪʟᴇs: <code>{}</code></b>
-<b>👥 ᴛᴏᴛᴀʟ ᴜsᴇʀs: <code>{}</code></b>
-<b>💭 ᴛᴏᴛᴀʟ ᴄʜᴀᴛs: <code>{}</code></b>
-<b>📠 ᴜsᴇᴅ sᴛᴏʀᴀɢᴇ: <code>{}</code></b>
-<b>🗂️ ғʀᴇᴇ sᴛᴏʀᴀɢᴇ: <code>{}</code></b>"""
-    LOG_TEXT_G = """#𝐍𝐞𝐰𝐆𝐫𝐨𝐮𝐩
-    
-<b>᚛› Group = {}(<code>{}</code>)</b>
-<b>᚛› Members = <code>{}</code></b>
-<b>᚛› Added By ⪼ {}</b>
+    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
+★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
+★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+    LOG_TEXT_G = """#NewGroup
+Group = {}(<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
 """
     LOG_TEXT_P = """#NewUser
-    
-<b>᚛› id - <code>{}</code></b>
-<b>᚛› Name - {}</b>
-"""
-    REPORT_TXT = """<b>Usage.</b>
-
-/report 𝗈𝗋 @admins """
-
-    CORONA_TXT = """/covid - Use This Command To know Covid informations.
-
-Example:
-
-<code>/covid India</code>"""
-
-    URLSHORT_TXT = """This Command will Help You To short a Link.
-
-<b>Usage: /short <link>
-
-Example:
-
-<code>/short https://t.me/ddrabit</code>"""
-
-    VIDEO_TXT ="""<b>Youtube Video Downloader.</b>
-
-<b>Usage:</b>
-
-Type: /video <link>
-Type: /mp4 <link>"""
-
-    ZOMBIES_TXT = """<b>Kick incative members from group. Add me as admin with ban users permission in group.</b>
-
-<b>Commands and Usage.</b>
-
-• /inkick - command with required arguments and i will kick members from group.
-• /instatus - to check current status of chat member from group.
-• /inkick within_month long_time_ago - to kick users who are offline for more than 6-7 days.
-• /inkick long_time_ago - to kick members who are offline for more than a month and Deleted Accounts.
-• /dkick - to kick deleted accounts."""
-
-    IMAGE_TXT = """<b>Commands and Usage.</b>
-
-• /imdb  - get the film information from IMDb source.
-• /search  - get the film information from various sources."""
-
-    STICKER_TXT = """<b>Usage.</b>
-
-/stickerid - Reply to any sticker for sticker id."""
-
-    YTTHUMB_TXT = """<b>Youtube Video Thumbnail Downloader.</b>
-
-Usage: /ytthumb <video link>
-
-Example: /ytthumb https://youtu.be/UyzJ9KEoU0w"""
-
-    ABOOK_TXT = """<b>You can convert a pdf file to a audio file with this command.</b>
-
-<b>Commands and Usage.</b>
-
-/audiobook - <b>Reply to PDF file to generate the audio.</b>"""
-
-    GTRANS_TXT = """<b>Help: Gᴏᴏɢʟᴇ Tʀᴀɴsʟᴀᴛᴏʀ
-
-Tʀᴀɴsʟᴀᴛᴇ ᴛᴇxᴛs ᴛᴏ ᴀ sᴘᴇᴄɪғɪᴄ ʟᴀɴɢᴜᴀɢᴇ!
-
-Cᴏᴍᴍᴀɴᴅs ᴀɴᴅ Usᴀɢᴇ: /tr [lang Code][reply] - ᴛʀᴀɴsʟᴀᴛᴇ ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ᴛᴏ sᴘᴇᴄɪғɪᴄ ʟᴀɴɢᴜᴀɢᴇ.
-
-NOTE:
-
-While Using /tr you should specify the language code.
-
-Example:
-
-• en = english
-• ml = malayalam
-• hi = hindi
-
-Type: /tr ml
-
-For More Language Codes Click Here 👇</b>"""
-
-    RESTRIC_TXT = """Some people need to be publicly banned; spammers, annoyances, or just trolls.
-
-This module allows you to do that easily, by exposing some common actions, so everyone will see!
-
-<b>Commands and Usage.</b>
-
-/ban - Ban a user.
-/unban - To unban a user.
-/tban - Temporarily ban a user. Example time values: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks.
-/mute - To mute a user.
-/unmute - To unmute a user.
-/tmute - Temporarily mute a user. Example time values: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks"""
-    CREATOR_REQUIRED = """<b>You have To Be The Group Owner To Do That.</b>"""
-      
-    INPUT_REQUIRED = "**Arguments Required.**"
-      
-    KICKED = """✔️ Successfully Kicked {} Members According To The Arguments Provided."""
-      
-    START_KICK = """🚮 Removing Inactive Members This May Take A While..."""
-      
-    ADMIN_REQUIRED = """<b>Im Not Admin Here. Add Me Again with all admin rights.</b>"""
-      
-    DKICK = """✔️ Kicked {} Deleted Accounts Successfully."""
-      
-    FETCHING_INFO = """<b>Fetching User info...</b>"""
-      
-    STATUS = """{}\n<b>Chat Member Status</b>**\n\n```<i>Recently``` - {}\n```Within Week``` - {}\n```Within Month``` - {}\n```Long Time Ago``` - {}\nDeleted Account - {}\nBot - {}\nUnCached - {}</i>
+ID - <code>{}</code>
+Name - {}
 """
