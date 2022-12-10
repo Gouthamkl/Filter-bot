d2333c9b4d2f0eb293478d66e3a5b21054e5133a
MikeTG000 <119830644+MikeTG000@users.noreply.github.com>
2022-12-10 13:39:05 +0530

    Update gfilters_mdb.py

--- database/gfilters_mdb.py
+++ database/gfilters_mdb.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 myclient = pymongo.MongoClient(DATABASE_URI)
-mydb = myclient["GlobalFilters"]
+mydb = myclient[DATABASE_NAME]
 
 
 
@@ -75,18 +75,18 @@ async def delete_gfilter(message, text, gfilters):
         )
     else:
         await message.reply_text("Couldn't find that gfilter!", quote=True)
-
+        
 async def del_allg(message, gfilters):
     if str(gfilters) not in mydb.list_collection_names():
-        await message.edit_text("Nothin!")
+        await message.edit_text("Nothing to Remove !")
         return
 
     mycol = mydb[str(gfilters)]
     try:
         mycol.drop()
-        await message.edit_text(f"All filters has been removed")
+        await message.edit_text(f"All gfilters has been removed !")
     except:
-        await message.edit_text("Couldn't remove all filters!")
+        await message.edit_text("Couldn't remove all gfilters !")
         return
 
 async def count_gfilters(gfilters):
