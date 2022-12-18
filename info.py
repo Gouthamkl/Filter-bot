ff9fffbd27e713320a7d232e73e0bf16378ed466
Gouthamkl <120896961+Gouthamkl@users.noreply.github.com>
2022-12-18 12:41:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 # Bot information
 PORT = environ.get("PORT", "8000")
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '18979569'))
+API_HASH = environ.get('API_HASH', '45db354387b8122bdf6c1b0beef93743')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5444919970:AAGDxD6pbvXiW6jvJaQgjwiYIXIHxvRgCGU')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -33,12 +33,12 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', '')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://A:A@cluster0.ff7jiih.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "A")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001729830342'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001716975795'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+tyfzYyUtPp4zYTE1')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'True')), False)
 IMDB = is_enabled((environ.get('IMDB', 'False')), False)
